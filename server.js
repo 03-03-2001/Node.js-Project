755f106b1c0f7a07a3a9ee606bcbc5e06a904729
03-03-2001 <rohitvkamble0301@gmail.com>
2026-06-13 01:45:19 +0530

    Fix server.js for Vercel deployment

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const PORT = process.env.PORT || 3000;
 
 
 
-module.exports = (req,res)=>{
-    handleRoute(req,res)
-}
+const server = http.createServer((req,res)=>{
+    handleRoute(req,res);
+})
+
+module.exports = server;
 
 if(require.main===module){
-    const server = http.createServer((req,res)=>{
-        handleRoute(req,res);
-    })
+  server.listen(PORT, () => {
+    console.log(`Server is Running at http://localhost:${PORT}`);
+}); 
 }
 
 
  
 
-server.listen(PORT, () => {
-    console.log(`Server is Running at http://localhost:${PORT}`);
-});
+
